31a7d39fb3d53cfdb19c9a88220d5cf4c9e4ccf6
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2026-04-19 19:19:02 +0200

    Add invalid password exception

---
+++ src/main/java/io/github/artshp/jwhisper/client/cli/exception/InvalidPasswordException.java
@@ -0,0 +1,10 @@
+package io.github.artshp.jwhisper.client.cli.exception;
+
+import lombok.experimental.StandardException;
+
+/**
+ * Exception for invalid provided password.
+ */
+@StandardException
+public class InvalidPasswordException extends RuntimeException {
+}
