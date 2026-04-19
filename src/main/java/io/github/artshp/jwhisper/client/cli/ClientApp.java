01dce7d1634c7a8e7e4dad94ab2e15af21795a05
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2026-04-19 18:50:48 +0200

    Update user data reading

--- src/main/java/io/github/artshp/jwhisper/client/cli/ClientApp.java
+++ src/main/java/io/github/artshp/jwhisper/client/cli/ClientApp.java
@@ -7,7 +7,9 @@ import java.security.KeyPair;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
+import java.util.Arrays;
 import java.util.HexFormat;
+import java.util.Optional;
 
 /**
  * Client app class.
@@ -41,25 +43,114 @@ class ClientApp {
         if (IdentityManager.isKeyStoreAvailable()) {
             log.info("Key Store is available. Trying to load it...");
 
-            char[] password = readPassword();
-            keyPair = IdentityManager.loadKeys(password);
+            Optional<char[]> password = readPassword(false);
+            if (password.isEmpty()) {
+                log.error("Failed to get password.");
+                throw new IllegalStateException("Failed to get password.");
+            }
+
+            keyPair = IdentityManager.loadKeys(password.get());
         } else {
             log.info("Key Store is not available. Creating it...");
 
-            char[] password = readPassword();
-            String username = readUsername();
-            keyPair = IdentityManager.createKeys(password, username);
+            Optional<char[]> password = readPassword(true);
+            if (password.isEmpty()) {
+                log.error("Failed to get password.");
+                throw new IllegalStateException("Failed to get password.");
+            }
+
+            Optional<String> username = readUsername();
+            if (username.isEmpty()) {
+                log.error("Failed to get username.");
+                throw new IllegalStateException("Failed to get username.");
+            }
+
+            keyPair = IdentityManager.createKeys(password.get(), username.get());
         }
 
         log.info("Identity loaded. Fingerprint: {}", calculateFingerprint(keyPair.getPublic()));
     }
 
-    private char[] readPassword() {
-        return console.readPassword("Enter Password: ");
+    private static boolean isPasswordValid(char[] password) {
+        return password.length >= 4;
     }
 
-    private String readUsername() {
-        return console.readLine("Enter Username: ");
+    private Optional<char[]> readPassword(boolean repeat) {
+        char[] password;
+
+        boolean isValid;
+        int i = 0;
+        do {
+            password = console.readPassword("Enter password: ");
+            if (!isPasswordValid(password)) {
+                isValid = false;
+                log.warn("Password length should be at least 4. Try again.");
+            } else {
+                isValid = true;
+            }
+
+            i++;
+        } while (!isValid && i < 3);
+
+        if (!isValid) {
+            log.error("Provided password is invalid.");
+            return Optional.empty();
+        }
+
+        if (!repeat) {
+            return Optional.of(password);
+        }
+
+        char[] passwordRetry;
+
+        i = 0;
+        do {
+            passwordRetry = console.readPassword("Re-enter password: ");
+            if (!Arrays.equals(password, passwordRetry)) {
+                isValid = false;
+                log.warn("Passwords are not equal. Try again.");
+            } else {
+                isValid = true;
+            }
+
+            i++;
+        } while (!isValid && i < 3);
+
+        if (!isValid) {
+            log.warn("You failed to repeat the password.");
+            return Optional.empty();
+        }
+
+        return Optional.of(password);
+    }
+
+    private static boolean isUsernameValid(String username) {
+        return username.length() >= 4;
+    }
+
+    private Optional<String> readUsername() {
+        String username;
+
+        boolean isValid;
+        int i = 0;
+        do {
+            username = console.readLine("Enter username: ").trim();
+            if (!isUsernameValid(username)) {
+                isValid = false;
+                log.warn("Username length should be at least 4. Try again.");
+            } else {
+                isValid = true;
+            }
+
+            i++;
+        } while (!isValid && i < 3);
+
+        if (!isValid) {
+            log.error("Provided username is invalid.");
+            return Optional.empty();
+        }
+
+        return Optional.of(username);
     }
 
     private static String calculateFingerprint(PublicKey key) {
