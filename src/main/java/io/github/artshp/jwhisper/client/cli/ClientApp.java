cb7004a05e12e8026c67d17522fadf64d0beb31e
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2026-04-19 10:31:28 +0200

    feat: add entry point (#2)

---
+++ src/main/java/io/github/artshp/jwhisper/client/cli/ClientApp.java
@@ -0,0 +1,14 @@
+package io.github.artshp.jwhisper.client.cli;
+
+/**
+ * Client app main class.
+ */
+class ClientApp {
+
+    /**
+     * Client app entry point.
+     */
+    static void main() {
+        System.out.println("Client started.");
+    }
+}
