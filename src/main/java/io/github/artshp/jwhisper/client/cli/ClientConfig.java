7e6b16d07d5db37f4c866982f4cfb9388d59dd91
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2026-05-02 15:52:06 +0200

    Add pretty print for config

--- src/main/java/io/github/artshp/jwhisper/client/cli/ClientConfig.java
+++ src/main/java/io/github/artshp/jwhisper/client/cli/ClientConfig.java
@@ -1,9 +1,17 @@
 package io.github.artshp.jwhisper.client.cli;
 
+import tools.jackson.databind.ObjectMapper;
+
 /**
  * Class representing client configuration.
  * @param hostname relay's hostname
  * @param port relay's port
  */
 public record ClientConfig(String hostname, int port) {
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    public String toPrettyString() {
+        return mapper.writeValueAsString(this);
+    }
 }
