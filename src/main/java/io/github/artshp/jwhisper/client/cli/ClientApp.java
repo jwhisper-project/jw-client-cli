14002a34cb41e184ccd800af1c2086c370ed9eee
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2026-05-09 17:32:13 +0200

    feat: add users registration (#5)

--- src/main/java/io/github/artshp/jwhisper/client/cli/ClientApp.java
+++ src/main/java/io/github/artshp/jwhisper/client/cli/ClientApp.java
@@ -2,7 +2,9 @@ package io.github.artshp.jwhisper.client.cli;
 
 import io.github.artshp.jwhisper.common.crypto.CertUtils;
 import io.github.artshp.jwhisper.common.exception.InputRetryException;
+import io.github.artshp.jwhisper.common.exception.NetworkServiceException;
 import io.github.artshp.jwhisper.common.exception.WrongPasswordException;
+import io.github.artshp.jwhisper.common.io.ConsoleUtils;
 import io.github.artshp.jwhisper.common.io.UserInputUtils;
 import lombok.extern.slf4j.Slf4j;
 
@@ -28,9 +30,11 @@ class ClientApp {
     /**
      * Start client application.
      */
-    public void start() throws InputRetryException {
+    public void start() throws InputRetryException, NetworkServiceException {
         log.info("Starting Client App");
         System.out.println("----- JWhisper Client -----");
+
+        String username = UserInputUtils.readUsername();
 
         KeyPair keyPair;
         if (IdentityManager.isKeyStoreAvailable()) {
@@ -47,7 +51,6 @@ class ClientApp {
             log.info("Key Store is not available. Creating it...");
 
             char[] password = UserInputUtils.readNewPassword();
-            String username = UserInputUtils.readUsername();
             keyPair = IdentityManager.createKeys(password, username);
         }
 
@@ -85,6 +88,18 @@ class ClientApp {
 
         try (NetworkClient client = new NetworkClient(serverTrustManager, config.hostname(), config.port())) {
             client.connect();
+            if (!client.register(username, keyPair)) {
+                throw new NetworkServiceException("Failed to register your user.");
+            }
+
+            String message;
+            do {
+                message = ConsoleUtils.readString("Mock chat: ", _ -> true, null, 1);
+            } while (!"quit".equals(message));
+
+            if (!client.unregister(username)) {
+                throw new NetworkServiceException("Failed to unregister your user.");
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/io/github/artshp/jwhisper/client/cli/NetworkClient.java
+++ src/main/java/io/github/artshp/jwhisper/client/cli/NetworkClient.java
@@ -1,12 +1,14 @@
 package io.github.artshp.jwhisper.client.cli;
 
 import io.github.artshp.jwhisper.common.crypto.SecurityUtils;
-import io.github.artshp.jwhisper.common.protocol.MessageTransport;
+import io.github.artshp.jwhisper.common.crypto.SigningUtils;
+import io.github.artshp.jwhisper.common.protocol.*;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import java.io.IOException;
+import java.security.KeyPair;
 
 @Slf4j
 public class NetworkClient implements AutoCloseable {
@@ -40,6 +42,59 @@ public class NetworkClient implements AutoCloseable {
             log.error("Error connecting to relay", e);
             throw new RuntimeException(e);
         }
+    }
+
+    public boolean register(String username, KeyPair keyPair) throws IOException {
+        byte[] signature = SigningUtils.sign(keyPair.getPrivate(), username.getBytes());
+        RegisterRequest request = new RegisterRequest(
+                username,
+                keyPair.getPublic().getEncoded(),
+                signature
+        );
+
+        send(request);
+        WhisperMessage response = receive();
+
+        if (response instanceof StatusResponse statusResponse) {
+            if (statusResponse.success()) {
+                log.info("Successfully registered user {}", username);
+                return true;
+            } else {
+                log.error("Failed to register user {}", username);
+            }
+        } else {
+            log.error("Unexpected response {}. Failed to register user {}", response, username);
+        }
+
+        return false;
+    }
+
+    public boolean unregister(String username) throws IOException {
+        UnregisterRequest request = new UnregisterRequest();
+
+        send(request);
+        WhisperMessage response = receive();
+
+        if (response instanceof StatusResponse statusResponse) {
+            if (statusResponse.success()) {
+                log.info("Successfully unregistered user {}", username);
+                return true;
+            } else {
+                log.error("Failed to unregister user {}", username);
+            }
+        } else {
+            log.error("Unexpected response {}. Failed to unregister user {}", response, username);
+        }
+
+        return false;
+    }
+
+    public void send(WhisperMessage message) throws IOException {
+        transport.sendMessage(socket.getOutputStream(), message);
+    }
+
+    public WhisperMessage receive() throws IOException {
+        return transport.receiveMessage(socket.getInputStream(), WhisperMessage.class);
     }
 
     @Override
