4872f0bd4b8edb7facc307bb899c7e099c6cb91d
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2026-05-17 15:42:03 +0200

    Add UserKeys javadoc

--- src/main/java/io/github/artshp/jwhisper/client/cli/users/UserKeys.java
+++ src/main/java/io/github/artshp/jwhisper/client/cli/users/UserKeys.java
@@ -2,5 +2,13 @@ package io.github.artshp.jwhisper.client.cli.users;
 
 import java.security.KeyPair;
 
-public record UserKeys(KeyPair signing, KeyPair encryption) {
+/**
+ * User's signing and encryption keys.
+ * @param signing signing key pair
+ * @param encryption encryption key pair
+ */
+public record UserKeys(
+        KeyPair signing,
+        KeyPair encryption
+) {
 }
