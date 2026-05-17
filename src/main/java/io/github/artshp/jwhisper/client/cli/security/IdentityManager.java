61dad015090bca07a8f794fd5a9a50beff3b2f5c
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2026-05-17 15:04:55 +0200

    Add IdentityManager javadoc

--- src/main/java/io/github/artshp/jwhisper/client/cli/security/IdentityManager.java
+++ src/main/java/io/github/artshp/jwhisper/client/cli/security/IdentityManager.java
@@ -22,18 +22,46 @@ import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.Date;
 
+/**
+ * Client identity manager. Responsible for client signing and encryption keys.
+ */
 @Slf4j
-public class IdentityManager {
+public final class IdentityManager {
 
+    /**
+     * Signing key alias in key store.
+     */
     private static final String SIGNING_KEY_ALIAS = "jwhisper-sign";
+
+    /**
+     * Encryption key alias in key store.
+     */
     private static final String ENCRYPTION_KEY_ALIAS = "jwhisper-encrypt";
+
+    /**
+     * Filename of key store with client keys
+     */
     private static final String KEYSTORE_FILE = "identity.p12";
+
+    /**
+     * Path to key store with client keys
+     */
     private static final Path KEYSTORE_FILE_PATH = Path.of(KEYSTORE_FILE);
 
+    /**
+     * Does key store file exist?
+     * @return {@code true} if key store file exists, otherwise {@code false}
+     */
     public static boolean isKeyStoreAvailable() {
         return Files.exists(KEYSTORE_FILE_PATH);
     }
 
+    /**
+     * Load client keys.
+     * @param password password to key store
+     * @return user keys
+     * @throws WrongPasswordException if wrong password provided
+     */
     public static UserKeys loadKeys(char[] password) throws WrongPasswordException {
         LOGGER.info("Trying to load existing key store from file \"{}\"", KEYSTORE_FILE_PATH);
 
@@ -48,13 +76,12 @@ public class IdentityManager {
         }
     }
 
-    private static KeyPair loadKeyPair(KeyStore keyStore, String alias, char[] password)
-            throws NoSuchAlgorithmException, KeyStoreException, UnrecoverableKeyException {
-        PrivateKey privateKey = (PrivateKey) keyStore.getKey(alias, password);
-        Certificate certificate = keyStore.getCertificate(alias);
-        return new KeyPair(certificate.getPublicKey(), privateKey);
-    }
-
+    /**
+     * Create client keys.
+     * @param password password to be used for truststore
+     * @param username username to be used for certificate generation
+     * @return user keys
+     */
     public static UserKeys createKeys(char[] password, String username) {
         LOGGER.info("Key store file \"{}\" does not exist. Creating a new one", KEYSTORE_FILE_PATH);
 
@@ -88,6 +115,30 @@ public class IdentityManager {
         return new UserKeys(signing, encryption);
     }
 
+    /**
+     * Load key pair from given key store.
+     * @param keyStore key store to load key pair from
+     * @param alias key pair alias
+     * @param password key store password
+     * @return loaded key pair
+     * @throws NoSuchAlgorithmException if the algorithm for recovering the key cannot be found
+     * @throws KeyStoreException if the keystore has not been initialized
+     * @throws UnrecoverableKeyException if the key cannot be recovered (e.g., the given password is wrong)
+     */
+    private static KeyPair loadKeyPair(KeyStore keyStore, String alias, char[] password)
+            throws NoSuchAlgorithmException, KeyStoreException, UnrecoverableKeyException {
+        PrivateKey privateKey = (PrivateKey) keyStore.getKey(alias, password);
+        Certificate certificate = keyStore.getCertificate(alias);
+        return new KeyPair(certificate.getPublicKey(), privateKey);
+    }
+
+    /**
+     * Generate self-signed {@code X509} certificate.
+     * @param subjectPublicKey public key for certificate
+     * @param signingKey private key for certificate
+     * @param username username for certificate
+     * @return generated certificate
+     */
     private static X509Certificate generateCertificate(PublicKey subjectPublicKey, PrivateKey signingKey, String username) {
         final long now = System.currentTimeMillis();
         X509v3CertificateBuilder certificateBuilder = getX509v3CertificateBuilder(subjectPublicKey, username, now);
@@ -107,6 +158,13 @@ public class IdentityManager {
         }
     }
 
+    /**
+     * Create {@code X509} certificate builder.
+     * @param subjectPublicKey public key for certificate
+     * @param username username for certificate
+     * @param now current time in UNIX format
+     * @return certificate builder
+     */
     private static X509v3CertificateBuilder getX509v3CertificateBuilder(
             PublicKey subjectPublicKey, String username, long now
     ) {
@@ -118,5 +176,9 @@ public class IdentityManager {
         return new JcaX509v3CertificateBuilder(
                 name, certificateSerialNumber, startDate, endDate, name, subjectPublicKey
         );
+    }
+
+    private IdentityManager() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
     }
 }
