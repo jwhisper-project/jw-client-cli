121e58738d1a1bd85297cc0b28167e3317412e9d
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2026-05-17 14:50:35 +0200

    Add NetworkClient javadoc

--- src/main/java/io/github/artshp/jwhisper/client/cli/network/NetworkClient.java
+++ src/main/java/io/github/artshp/jwhisper/client/cli/network/NetworkClient.java
@@ -15,22 +15,52 @@ import javax.net.ssl.SSLSocketFactory;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
+/**
+ * Network client.
+ */
 @Slf4j
 public class NetworkClient implements AutoCloseable {
 
+    /**
+     * Service responsible for network communication.
+     */
     private final MessageTransport transport = new MessageTransport();
+
+    /**
+     * Trust manager needed to trust only to the white list of servers.
+     */
     private final ServerTrustManager trustManager;
+
+    /**
+     * Relay's hostname.
+     */
     private final String host;
+
+    /**
+     * Relay's port.
+     */
     private final int port;
 
+    /**
+     * Client socket.
+     */
     private SSLSocket socket;
 
+    /**
+     * Create a new network client.
+     * @param trustManager server trust manager
+     * @param host relay's hostname
+     * @param port relay's port
+     */
     public NetworkClient(ServerTrustManager trustManager, String host, int port) {
         this.trustManager = trustManager;
         this.host = host;
         this.port = port;
     }
 
+    /**
+     * Connect to relay server.
+     */
     public void connect() {
         LOGGER.info("Connecting to relay at {}:{}...", host, port);
 
@@ -49,6 +79,13 @@ public class NetworkClient implements AutoCloseable {
         }
     }
 
+    /**
+     * Register user on the server.
+     * @param username user's username
+     * @param keys user's signing and encryption keys for network communication
+     * @return {@code true} if user was registered successfully, otherwise {@code false}
+     * @throws IOException if failed to send request or failed to receive response.
+     */
     public boolean register(String username, UserKeys keys) throws IOException {
         byte[] signature = SigningUtils.sign(
                 keys.signing().getPrivate(),
@@ -78,14 +115,28 @@ public class NetworkClient implements AutoCloseable {
         return false;
     }
 
+    /**
+     * Send message to server.
+     * @param message message to send
+     * @throws IOException if failed to send message
+     */
     public void send(WhisperMessage message) throws IOException {
         transport.sendMessage(socket.getOutputStream(), message);
     }
 
+    /**
+     * Receive message from server.
+     * @return received message
+     * @throws IOException if failed to receive message
+     */
     public WhisperMessage receive() throws IOException {
         return transport.receiveMessage(socket.getInputStream(), WhisperMessage.class);
     }
 
+    /**
+     * Stop client, close connection to server.
+     * @throws IOException if an I/O error occurs when closing the socket
+     */
     @Override
     public void close() throws IOException {
         LOGGER.info("Closing connection to relay");
